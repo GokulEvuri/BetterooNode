3283cba25e7a3edca8adf4ceb866d8594a77811d
Gokul Evuri <gokul@evuri.com>
2014-03-08 18:55:12 +0100

    adding login and register pages for production

--- betteroo_production/app.js
+++ betteroo_production/app.js
@@ -15,6 +15,12 @@ app.use("/images", express.static(__dirname + '/images'));
 app.get("/", function (req, res) {
   res.render('index');
 });
+app.get("/login", function (req, res) {
+  res.render('login');
+});
+app.get("/register", function (req, res) {
+  res.render('register');
+});
 
 app.listen(8090, function () {
   console.log("starting server");
