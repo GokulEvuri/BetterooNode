eda846917af9c03d6ee723da45bbbaa1f958b472
Gokul Evuri <gokul@evuri.com>
2014-03-18 22:35:25 +0100

    adding user object literal

--- betteroo_production/app.js
+++ betteroo_production/app.js
@@ -1,27 +1,54 @@
-var express = require("express");
-var app = express();
+// Author: Gokul Sai Evuri (gokul@betteroo.com)
 
-var mustacheExpress = require('mustache-express');
+//var express = require("express");
+//var app = express();
+var AWS = require('aws-sdk');
+
+//var mustacheExpress = require('mustache-express');
+
+//**
+// AWS Configuring
+//**//
+AWS.config.loadFromPath('./config.json');
+
+	// Creating object for dynamoDB
+var dynamodb = new AWS.DynamoDB();
+// AWS Config. END
+
 
 // Register '.mustache' extension with The Mustache Express
-app.engine('mustache', mustacheExpress());
+//app.engine('mustache', mustacheExpress());
 
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+//app.set('view engine', 'mustache');
+//app.set('views', __dirname + '/views');
 
-app.use("/tools", express.static(__dirname + '/tools'));
-app.use("/images", express.static(__dirname + '/images'));
+//app.use("/tools", express.static(__dirname + '/tools'));
+//app.use("/images", express.static(__dirname + '/images'));
 
-app.get("/", function (req, res) {
-  res.render('index');
-});
-app.get("/login", function (req, res) {
-  res.render('login');
-});
-app.get("/register", function (req, res) {
-  res.render('register');
-});
+//app.get("/", function (req, res) {
+//  res.render('index');
+//});
+//app.get("/login", function (req, res) {
+//  res.render('login');
+//});
+//app.get("/register", function (req, res) {
+//  res.render('register');
+//});
 
-app.listen(8090, function () {
-  console.log("starting server");
+//app.listen(8090, function () {
+//  console.log("starting server");
+//});
+
+var item = {
+	"id":{"N":1234},
+	"title":{"S":"FooBar"}
+
+}
+
+dynamodb.putItem({TableName: 'log_dev', Item: item}, function(err, data){
+    if (err) {
+    console.log(err); // an error occurred
+    } else {
+    console.log(data); // successful response
+    }
 });
