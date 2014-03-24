2afcbc583ecac1943c0e910777eb29412f194da3
Gokul Evuri <gokul@evuri.com>
2014-03-25 00:10:25 +0100

    post data for AWS SDK poll data posting

---
+++ betteroo_production/post_data.js
@@ -0,0 +1,14 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: 'AKIAJ3ZCZ53KG6X4EYWA', 
+  secretAccessKey: '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',
+  region: 'eu-west-1'
+});
+
+var dynamodb = new AWS.DynamoDB();
+dynamodb.describeTable(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
