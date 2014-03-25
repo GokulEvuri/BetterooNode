e204c3747d5d8564901c5a4dc637af3eeebb318e
Gokul Evuri <gokul@evuri.com>
2014-03-25 01:51:44 +0100

    added data types user registration

---
+++ betteroo_production/betteroo_api.js
@@ -0,0 +1,13 @@
+//**** Abstract Functions ****//
+function register_user(userobject){
+
+}
+
+
+//**** LOW-LEVEL FUNCTIONS ****//
+
+// to create a new user in database
+function create_user(userobject){
+
+}
+
--- betteroo_production/datatypes.js
+++ betteroo_production/datatypes.js
@@ -1,4 +1,4 @@
-var User = {
+var user = {
 	user_id: "value", 
 	user_name: "value", 
 	password: "value", 
@@ -17,5 +17,35 @@ var User = {
 	user_twitter_id: "value", 
 	is_prof: "value", // Is a paid user or belongs to paid company
 	name_of_company: "value",
-	location: "value"
+	location: "value",
+	posts_own:{} // Should contain post_id of posts
+	user_ip_history: {}, // Ip adresses of user activity
+	last_login:{}, // time stamp of last login
+	time_spent: "value", 
+	followers:{}, // Should contain user_id
+	following:{}  // Should contain user_id
 };
+
+
+var post = {
+	post_id: "value", // Unique id for each post
+	post_images_refs: {}, // Image reference links on S3
+	post_options_count: "value", // Integer, how many options available in post
+	post_options_stats:{} // Should contain post_stats datatype objects for each post option 
+	post_owner: "value"
+};
+
+var post_stats = {
+	stats_choosen_users: {}
+};
+
+var choosen_users = {
+	user_id: "value",
+	time_of_response: "value",
+	ip_of_user: "value" 
+}
+
+¨var user_struct = JSON.stringify(user);
+¨var post_struct = JSON.stringify(post);
+¨var post_stats_struct = JSON.stringify(post_stats);
+¨var choosen_users_struct = JSON.stringify(choosen_users);
