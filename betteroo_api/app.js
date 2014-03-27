102fe62ab2cbcbf00316904cc84704acf64650ec
Gokul Evuri <gokul@evuri.com>
2014-03-27 12:57:28 +0100

    Scheduled back up

---
+++ betteroo_api/app.js
@@ -0,0 +1,33 @@
+// Author: Gokul Sai Evuri (gokul@betteroo.com)
+
+var express = require("express");
+var app = express();
+var AWS = require('aws-sdk');
+
+
+//**
+// AWS Configuring
+//**//
+AWS.config.loadFromPath('./config.json');
+
+// Creating object for dynamoDB
+var dynamodb = new AWS.DynamoDB();
+// AWS Config. END
+
+app.use(express.logger('dev'));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(app.router);
+// assuming POST: name=foo&color=red            <-- URL encoding
+//
+// or       POST: {"name":"foo","color":"red"}  <-- JSON encoding
+
+app.post('/register', function(req, res) {
+    var name = req.body.name,
+        color = req.body.color;
+    // ...
+});
+
+app.listen(8090, function () {
+  console.log("starting server");
+});
